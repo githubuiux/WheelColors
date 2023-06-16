7850827b455d36d4d7be35f981f73fadc4ad1445
githubuiux <uiuxista@gmail.com>
2023-06-16 16:35:45 +0300

    Initial Commit

---
+++ WheelColors/WheelColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheelColorsApp.swift
+//  WheelColors
+//
+//  Created by Levent on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct WheelColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
